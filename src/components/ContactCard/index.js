90aafe1f5540bb49a023f49cfbda23aed2483901
Parthy <p.thaniperumkarunai@gmail.com>
2017-07-05 19:47:42 +0200

    more Cleanup

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -7,8 +7,6 @@ import ContactEntries from './components/ContactEntries'
 class ContactCard extends Component {
   state = {showEntry: 'email'}
 
-  onSelect = showEntry => this.setState({showEntry});
-
   render () {
     const {email, phone, gender, location, dob, picture, name} = this.props.entries[0];
     const {showEntry} = this.state;
@@ -16,7 +14,7 @@ class ContactCard extends Component {
     return (<Paper style={{width: '800px', height: '450px', marginTop: '50px'}}>
       <Avatar image={picture.large} {...name}/>
       <Entry type={showEntry} data={this.props.entries[0][showEntry]}/>
-      <ContactEntries entries={{email, phone, gender, location, dob}} onSelect={this.onSelect}/>
+      <ContactEntries entries={{email, phone, gender, location, dob}} onSelect={showEntry => this.setState({showEntry})}/>
     </Paper>);
   }
 }
